1e845d5596737cb65671631e47b561044f705f58
sudofly <deanayres@gmail.com>
2019-01-30 12:14:30 +0000

    Add files via upload

--- wikiwordlist.sh
+++ wikiwordlist.sh
@@ -13,16 +13,16 @@ else
 fi
 echo "looking for $today in dump file"
 
-if wget http://wikimedia.bytemark.co.uk/enwiki/$today/enwiki-$today-pages-articles.xml.bz2 > /dev/null 2>&1; then
+if wget -q --show-progress http://wikimedia.bytemark.co.uk/enwiki/$today/enwiki-$today-pages-articles.xml.bz2; then
 		echo  "downloaded enwiki-$today-pages-articles.xml.bz2"
 	else
-		echo "there is no file for $today. append the date to the script to download an alternative"
-	exit 1
+		echo "there is no file for $today. append the date from wikimedia.bytemark.co.uk/enwiki/ to the script to download an alternative"
+	#exit 1
 fi
 
 echo unzipping downloaded file 
-#bzip2 -d enwiki-20190101-pages-articles1.xml-p10p30302.bz2
-bzip2 -d enwiki-$today-pages-articles.xml.bz2
+bzip2 -d enwiki-20190101-pages-articles1.xml-p10p30302.bz2
+#bzip2 -d enwiki-$today-pages-articles.xml.bz2
 XMLFILE=$(ls enwiki*)
 
 echo "Splitting File "$XMLFILE" ...."
@@ -42,33 +42,34 @@ cd wikitmp
 echo making all spaces into linefeeds
 /usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/\s+/\n/g' wiki_*
 
+#echo "Removing words with a digit"
+#/usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/\S*\d+\S*//g' wiki_*
+#the reverse of above perl -pi -e 's/\b[^\d\W]+\b//g' wiki_*
+#echo ""
+
 echo "Removing non Alpha Characters"
 #might want to remove all characters 
 #perl -pi -e 's/[\W_$\[\]]+/\n/g' wiki_*
-
-#this removes the whole word
-usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/\w*([^\w\s]|\d)+\w* ?//g' wiki_*
+#this removes the whole word including the digit
+/usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/\w*([^\w\s]|\d)+\w* ?//g' wiki_*
 echo ""
 
 echo "Removing long words"
-usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/.{31,}//g' wiki_*
+/usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/.{31,}//g' wiki_*
 echo ""
 
 echo "Mutating UC to LC"
-usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 'tr/A-Z/a-z/' wiki_*
+/usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 'tr/A-Z/a-z/' wiki_*
 echo ""
 
 echo "Removing words less than 3 characters long"
-usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/\b.{1,3}\b//g' wiki_*
+/usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/\b.{1,3}\b//g' wiki_*
 echo ""
 
-echo "Removing words with a digit"
-usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e 's/\S*\d+\S*//g' wiki_*
-#the reverse of above perl -pi -e 's/\b[^\d\W]+\b//g' wiki_*
-echo ""
+
 
 echo "Removing words without vowels"
-usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e "s/\b[^aeiouy]*\b//g" wiki_*
+/usr/bin/time --format=' \n---- \nelapsed time is %e' perl -pi -e "s/\b[^aeiouy]*\b//g" wiki_*
 echo ""
 
 echo "Removing words with more than two consecutive letters"
